63d0c0e83abff5e0642dd4de1b079909a53490e3
Brook Beyene <brookbeyene37@gmail.com>
2021-02-16 07:51:02 -0700

    initial commit

---
+++ client/src/Components/Form/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const posenet = require('@tensorflow-models/posenet');
+
+async function estimatePoseOnImage(imageElement) {
+  // load the posenet model from a checkpoint
+  const net = await posenet.load();
+
+  const pose = await net.estimateSinglePose(imageElement, {
+    flipHorizontal: false
+  });
+  return pose;
+}
+
+const imageElement = document.getElementById('cat');
+
+const pose = estimatePoseOnImage(imageElement);
+
+console.log(pose);
